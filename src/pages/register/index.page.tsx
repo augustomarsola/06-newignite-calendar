dcc8caafbcad6edfb03e3856539221585dd0ac39
Augusto Marsola <augusto.marsola@live.com>
2023-09-05 21:39:44 -0300

    Added user creation

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -42,8 +43,15 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  const handleRegister = (data: RegisterFormData) => {
-    console.log(data)
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
